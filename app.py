ae9c9f646849c6713c9ad5ebb2ea7dc6d01ce001
MarkoMihalicka <mihalicka86@gmail.com>
2026-03-29 10:55:05 +0200

    app.py

--- app.py
+++ app.py
@@ -10,51 +10,61 @@ databaza = {
             "name": "Adrian",
             "surname": "Červenka",
             "nickname": "chilli peppers"
+            "img" = "https://www.odzadu.sk/wp-content/uploads/2026/03/adrian-zo-sou-ruza-pre-nevestu.jpg"
         }, {
             "id": 2,
-            "name": "karolína",
-            "surname": "Kmeťová",
+            "name": "Janka",
+            "surname": "Špenáová",
             "nickname": None
+            "img" = "https://www.stvr.sk/media/a501/image/file/1/1000/janka-pcs.jpg"
         }, {
             "id": 3,
             "name": "Markus",
             "surname": "Martiš",
             "nickname": "cigga"
+            "img" = "https://pbs.twimg.com/media/GYpgQMJXQAAtqkP.jpg"
         }, {
             "id": 4,
             "name": "Elizabeth",
             "surname": "RolsRojs",
             "nickname": "queen"
+            "img" = "https://www.fotoaparat.cz/storage/pm/05/08/03/126341_574c9.jpg"
         }, {
             "id": 5,
             "name": "Versace",
             "surname": "Klúčenka",
             "nickname": "Gucci"
+            "img" = "https://cdn.britannica.com/24/270724-050-ADD7DC96/donatella-versace-2024-vanity-fair-oscar-party-march-10-2024-beverly-hills-california.jpg"
         }, {
             "id": 6,
             "name": "Ctibor",
             "surname": "Cyril",
             "nickname": "Čvajgla"
+            "img" = "https://www.asb.sk/wp-content/uploads/2023/01/ASB_05_10_2022_-6-of-9-min-e1669667094611.jpg"
         }, {
             "id": 7,
             "name": "Lukáš",
             "surname": "Sfúkaš",
             "nickname": None
+            "img" = "https://upload.wikimedia.org/wikipedia/commons/3/34/Luk%C3%A1%C5%A1_Latin%C3%A1k_2015.jpg"
         }, {
             "id": 8,
             "name": "Roman",
             "surname": "Evka",
             "nickname": "detičky krásne"
+            "img" = "https://img.topky.sk/320px/1039568.jpg"
         }, {
             "id": 9,
             "name": "Tomáš",
             "surname": "Maštalír",
             "nickname": "herec"
+            "img" =
         }, {
             "id": 10,
             "name": "Patrik",
             "surname": "Vrbovský",
             "nickname": "Rytmus"
+            "img" = "https://i1.sndcdn.com/avatars-000003218454-hyqoka-t1080x1080.jpg"
         }
 
     ]
